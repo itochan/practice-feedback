05438c92414e2f1731bba4d3378b3d1a69bd3a39
Kazunori Jo <itochan315@gmail.com>
2016-01-02 00:36:52 +0900

    Generate access hash

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,3 +1,10 @@
 class Practice < ApplicationRecord
+  before_create :generate_access_hash
+
   mount_uploader :file, PracticeUploader
+
+  private
+  def generate_access_hash
+    self.access_hash = SecureRandom.hex
+  end
 end
